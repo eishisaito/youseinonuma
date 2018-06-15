6d953886e382c62cd9678cb74fafa3f4fdd702c4
eishi <saito@eishis.com>
2018-06-15 21:01:24 +0900

    messageのajax処理

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,22 @@
 class MessagesController < ApplicationController
+
+
+
+  def create
+    message = Message.new(message_params)
+    if message.save!
+      respond_to do |format|
+        format.html
+        format.js
+      end
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(
+      :body
+    )
+  end
+
 end
