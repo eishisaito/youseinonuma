edfaadc495c26c4dd134f7c6e4ea21d49c523bbd
eishi <saito@eishis.com>
2018-06-15 21:35:53 +0900

    .env追加

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'jquery-rails'
 gem 'slack-notifier'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'dotenv-rails'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,10 @@
 class MessagesController < ApplicationController
 
-
-
   def create
     message = Message.new(message_params)
     if message.save!
+      notifier = Slack::Notifier.new(ENV['SLACK_URL'] )
+      notifier.ping('Hello')
       respond_to do |format|
         format.html
         format.js
@@ -18,5 +18,4 @@ class MessagesController < ApplicationController
       :body
     )
   end
-
 end
